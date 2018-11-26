c3af5c5502e2bb7fbd7819c97dd52e74bda29f5b
Viktor27 <kolesnik.27091993@gmail.com>
2018-11-27 00:30:02 +0200

    commit me please

---
+++ work_1.js
@@ -0,0 +1,17 @@
+/*  Начальные 3 цифры в каждом номере- это код оператора
+ Сделать вывод  в консоль таким
+(063) 111 1234
+(333) 456 8768
+(444) 657 5547
+*/
+console.log('======= 1st home task =======');
+let st = `063-111-1234 (333)456-8768 4446575547`;
+let separator = " ";
+let numbers = st.split(separator);
+let reg = /(\d{0,3})(\d{0,3})(\d{0,4})/;
+
+for (let i = 0; i < numbers.length; i++) {
+    const number = numbers[i].replace(/\D/g, '').match(reg);
+    let formated = !number[2] ? number[1] : '(' + number[1] + ') ' + number[2] + (number[3] ? ' ' + number[3] : '');
+    console.log(formated);
+}
